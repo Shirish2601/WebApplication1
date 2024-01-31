181169f432bd8f46e5ce1e7586abf6bc2fc32456
shirish2601 <you@example.com>
2024-02-01 00:28:20 +0530

    added file data into class

--- Models/Reader/FileReader.cs
+++ Models/Reader/FileReader.cs
@@ -10,23 +10,56 @@ namespace Models.Reader
     {
         public void Read(string? path)
         {
-            if (path is not null) 
+            if (path is not null)
             {
-                string? file = string.Empty;
-                string? currentLine = string.Empty;
+                List<string> machineList = new();
+                List<Machine> machines = new();
                 using (StreamReader sr = new StreamReader(path))
                 {
-                    currentLine = sr.ReadLine();
-                    file += currentLine;
-                    file += "\n";
-                    while (currentLine != null)
+                    string? current = sr.ReadLine();
+                    while (current != null)
                     {
-                        currentLine = sr.ReadLine();
-                        file += currentLine;
-                        file += "\n";
+                        current = sr.ReadLine();
+                        string[]? contents = current?.Split(",");
+                        var machineName = contents?[0];
+                        if (machineName is not null && !machineList.Contains(machineName))
+                        {
+                            var asset = new Asset
+                            {
+                                AssetName = contents?[1],
+                                SeriesNumber = contents?[2]
+                            };
+                            var machine = new Machine
+                            {
+                                MachineName = machineName,
+                                Assets = new() { asset }
+                            };
+                            machineList.Add(machineName);
+                            machines.Add(machine);
+                        }
+                        else
+                        {
+                            var asset = new Asset
+                            {
+                                AssetName = contents?[1],
+                                SeriesNumber = contents?[2]
+                            };
+                            var currentMachine = machines.Find(machine => machine.MachineName == machineName);
+                            currentMachine?.Assets?.Add(asset);
+                        }
                     }
+                    foreach (var item in machines)
+                    {
+                        Console.Write($"{item.MachineName},");
+                        foreach (var asset in item?.Assets ?? Enumerable.Empty<Asset>())
+                        {
+                            Console.WriteLine($"{asset.AssetName}, {asset.SeriesNumber}");
+                        }
+                    }
+
+                    Console.WriteLine("--------------------------------------");
+                    Console.ReadLine();
                 }
-                Console.WriteLine(file);
             }
         }
     }
